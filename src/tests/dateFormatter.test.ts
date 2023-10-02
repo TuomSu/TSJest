2efdc9b91541a7f7ab2d58a1bd4b16350009f599
Susanna <susanna.tuomi@myy.haaga-helia.fi>
2023-10-02 11:04:46 +0300

    Osa 1 omat testit

--- src/tests/dateFormatter.test.ts
+++ src/tests/dateFormatter.test.ts
@@ -4,6 +4,34 @@ import { strict as assert } from 'node:assert';         // https://nodejs.org/ap
 import { finnishDateString } from '../dateFormatter';   // the function to be tested
 
 
-test('remove this test when writing your own tests', () => {
-    assert.fail('no tests written');
+//test('remove this test when writing your own tests', () => {
+//    assert.fail('no tests written');
+//});
+
+test('testing if dayname is translated to Finnish', () =>{
+const date = new Date('2023-10-02');
+const result = finnishDateString(date);
+const dayname = result.substring(0,9);
+assert.equal(dayname, 'maanantai');
 });
+
+test('testing if monthname is translated to Finnish', () =>{
+    const date = new Date('2023-10-02');
+    const result = finnishDateString(date);
+    const month = result.slice(-14,-5);
+    assert.equal(month, 'lokakuuta');
+    });
+
+test('testing daynumber', () =>{
+    const date = new Date('2023-10-02');
+    const result = finnishDateString(date);
+    const day = result.substring(10, 11);
+    assert.equal(day, '2');
+    });
+        
+test('testing year', () =>{
+    const date = new Date('2023-10-02');
+    const result = finnishDateString(date);
+    const year = result.slice(-4);
+    assert.equal(year, '2023');
+    });
